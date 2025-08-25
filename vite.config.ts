32453373d1cd7fcfd6eca8f55e3acdb769ab23fc
chaniii999 <psc0729@naver.com>
2025-08-25 15:49:14 +0900

    타입스크립트 설정 및 Vite 구성 개선: @types/node 추가, 경로 별칭 설정으로 코드 가독성 향상

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': '/src',
+      '@pages': '/src/ui/pages',
+      '@components': '/src/lib/components',
+      '@lib': '/src/lib',
+      '@store': '/src/store',
+      '@styles': '/src/styles',
+      '@utils': '/src/utils',
+    },
+  },
   server: {
     port: 5173,
     open: true,
